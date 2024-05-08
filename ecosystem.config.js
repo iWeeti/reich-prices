245a1238f3e65bae4cbbe68f1f346df98a106080
iWeeti <40580113+iWeeti@users.noreply.github.com>
2024-05-08 11:35:34 +0300

    refact: update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,6 @@ module.exports = {
         name: "Reich Prices Bot",
         script: "dist/index.js",
         watch: true,
-        ignore_watch: ["[\/\\]\./", "node_modules"]
+        ignore_watch: ["[\/\\]\./", "node_modules", 'db']
     }]
 }
